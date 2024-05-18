b9bd85332105182cef67f29724f17feefb00c2d2
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2024-05-18 12:11:33 +0530

    Submit Button Finish

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -46,7 +46,7 @@ class _LoginPageState extends State<LoginPage> {
                   ]),
             ),
             Container(
-              margin: const EdgeInsets.only(top: 50.0),
+              margin: const EdgeInsets.only(top: 80.0),
 
               color: const Color(0xFF071E3D),
               child: Column(
@@ -67,6 +67,11 @@ class _LoginPageState extends State<LoginPage> {
                       margin: const EdgeInsets.all(10.0),
                       padding: const EdgeInsets.only(left: 20, right: 20),
                       child: const TextField(
+                        style: TextStyle(
+                          color: Color(0xFFFFFFFF),
+                          fontFamily: 'Euclid-Circular-A',
+                          fontSize: 20,
+                        ),
                         decoration: InputDecoration(
                         labelText: 'USERNAME',
                           labelStyle: TextStyle(
@@ -77,31 +82,68 @@ class _LoginPageState extends State<LoginPage> {
                         enabledBorder: UnderlineInputBorder(
                           borderSide: BorderSide(width: 0),
                         ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(width: 0),
+                        ),
                       ),
                     ),
-                    ),Container(
+                    ),
+                    Container(
                       decoration: BoxDecoration(
                         color: const Color(0xFF11405c),
                         borderRadius: BorderRadius.circular(30),
                       ),
-                      margin: const EdgeInsets.all(10.0),
+                      margin: const EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0, bottom: 40.0),
                       padding: const EdgeInsets.only(left: 20, right: 20),
                       child: const TextField(
                         decoration: InputDecoration(
                           labelText: 'PASSWORD',
-                          labelStyle: TextStyle(
-                            color: Color(0xFFFFFFFF),
-                            fontFamily: 'Euclid-Circular-A',
-                            fontSize: 25,
+                            labelStyle: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontFamily: 'Euclid-Circular-A',
+                              fontSize: 25,
+                            ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(width: 0),
                           ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(width: 0),
+                          ),
+                        ),
 
+                      ),
+
+                    ),
+                    SizedBox(
+                      height: 60,
+                      width: 300,
+                      child: ElevatedButton(
+
+                      style: ButtonStyle(
+
+                        backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF158a84)),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {null;
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(builder: (context) => const HomePage()),
+                        // );
+                      },
+
+                      child: const Text('SUBMIT',
+                        style: TextStyle(
+                          fontFamily: 'Euclid-Circular-A',
+                          fontSize: 30,
+                          color: Color(0xFFffffff),
                         ),
                       ),
                     ),
-                    // ElevatedButton(
-                    //
-                    //   child: const Text('Submit'),
-                    // ),
+                    ),
                   ]),
             ),
             ],
